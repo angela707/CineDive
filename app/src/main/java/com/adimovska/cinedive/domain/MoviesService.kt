42b2d2ad8ae389daa1554fd39cb85cc48c08667a
angeladimovska <angela.dimovska@echo.com.mk>
2025-01-29 10:32:11 +0100

    remove unused classes

--- app/src/main/java/com/adimovska/cinedive/domain/MoviesService.kt
+++
@@ -1,8 +0,0 @@
-package com.adimovska.cinedive.domain
-
-import com.adimovska.cinedive.data.remote.dto.MovieDTO
-
-interface MoviesService {
-    suspend fun getMovies(page: Int = 1): List<MovieDTO>
-    suspend fun searchMovies(page: Int = 1, searchQuery: String): List<MovieDTO>
-}
